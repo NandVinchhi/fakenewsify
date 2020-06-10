11b2d8073a79cd0c484fc0b2b44a3ee98690dd23
Nand Vinchhi <58524154+NandVinchhi@users.noreply.github.com>
2020-06-10 22:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,21 @@ import dash_core_components as dcc
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+
 
 app.layout = html.Div([
 	html.H3("Fake News Classifier", style = {'color':'#696969'}),
-	html.P("This is an open source fake news classifier, made with python scikit-learn library and plotly dash front-end. It takes the title and text content of a news article as its inputs and classifies it into fake or real. check the github repo for more info: ", style = {'color':'#696969'}),
-	html.A("GitHub repo", href='https://github.com/NandVinchhi/fakenewsify', target="_blank"),
+	html.P("This is an open source fake news classifier, made with python scikit-learn library and plotly dash front-end. It takes the title and text content of a news article as its inputs and classifies it into fake or real. This project was made for the purpose of RookieHacks 2020. check the github repo for more info: ", style = {'color':'#696969'}),
+	html.A("GitHub repo", href='https://github.com/NandVinchhi/FakeNewsClassifier', target="_blank"),
 	
 	
 	html.Div([dcc.Textarea(
         id='title',
         value='',
         style={'width': '100%', 'height': 20},
-        placeholder = 'Enter title',
+        placeholder = 'Enter URL of Article',
     )], style={'marginTop': 25}),
-    html.Div([dcc.Textarea(
-        id='text',
-        value='',
-        placeholder = 'Enter text',
-        style={'width': '100%', 'height': 200},
-    )]),
+    
     html.Div([html.Button('Submit', id='button', n_clicks=0)]),
     html.Div(id='output', style={'whiteSpace': 'pre-line'})
 ], style={'marginBottom': 25, 'marginTop': 25, 'marginLeft':25, 'marginRight':400})
@@ -34,13 +29,16 @@ app.layout = html.Div([
 @app.callback(
     Output('output', 'children'),
     [Input('button', 'n_clicks')],
-    [State('title', 'value'), State('text', 'value')],
+    [State('title', 'value')],
     
     
 )
-def update_output(n_clicks, value, value2):
+def update_output(n_clicks, value):
+    
+
     if n_clicks > 0:
-        return "Prediction: " + predict.predict(value, value2)
+        k = predict.predict(value)
+        return k[0] + "\n" + "This article is " + k[1] + " and " + k[2] + ".\nSummary:\n" + k[3] 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
